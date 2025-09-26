df9c006f628a3ea36a1c4b086fc3f9caa9511b7c
Daniel Edema <Ogagaedema@gmail.com>
2025-09-26 20:13:02 +0000

    change logo to a coin like logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,19 +4,18 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        fill="currentColor"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         {...props}
     >
-        <circle cx="12" cy="12" r="10" className="text-primary" />
+        <circle cx="12" cy="12" r="10" className="text-primary" fill="currentColor" />
         <path
-            fillRule="evenodd"
-            d="M13.245 7.772a.75.75 0 00-1.066.976 3.003 3.003 0 01-1.34 3.252.75.75 0 10.72 1.302A4.503 4.503 0 0013.5 9.12V7.875a.75.75 0 00-.255-.103z"
-            clipRule="evenodd"
-            className="fill-primary-foreground"
+            d="M15.5 8.5a3 3 0 0 0-3-3h-2a3 3 0 0 0-3 3v7a3 3 0 0 0 3 3h2a3 3 0 0 0 3-3v-2"
+            className="stroke-primary-foreground"
         />
-        <path
-            d="M12 4.5a7.5 7.5 0 11-7.05 10.373A.75.75 0 005.82 14.1a6 6 0 105.74-7.925.75.75 0 00-.56-1.425z"
-            className="fill-primary-foreground"
-        />
+        <path d="M7.5 12.5h5" className="stroke-primary-foreground" />
     </svg>
 );
